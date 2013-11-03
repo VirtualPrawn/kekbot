3a21e741e8b2cc1e5617f0f436718e13d5f71967
ivan <strategetical@gmail.com>
2013-11-02 20:14:59 -0500

    mod_checks_and_enables Now the bot will do constant listening, but will not run certain commands when the bot is "disabled".

--- kekbot.js
+++ kekbot.js
@@ -1,43 +1,90 @@
 var kekbot = {};
 kekbot.enabled = false;
 kekbot.name = "kekbot";
-kekbot.enable = function(){
-	if(!kekbot.enabled){
-		API.on(API.CHAT, kekbot.handleCommand);
-		kekbot.say("["+kekbot.name+"] Enabled.");
-		kekbot.enabled = true;	
-	}
-	else{
-		console.log("KekBot is already enabled!");
-	}
+
+kekbot.mods = {};
+kekbot.mods["KekBot"] = "admin";
+
+kekbot.init = function(){
+	API.off(API.CHAT, kekbot.handleCommand);
+	API.on(API.CHAT, kekbot.handleCommand);
 }
-kekbot.say = function(msg){
-	$("#chat-input-field").val(msg).trigger($.Event("keydown",{keyCode: 13}));
-}
-kekbot.disable = function(){
-	if(kekbot.enabled){
-		API.off(API.CHAT, kekbot.handleCommand);
-		kekbot.say("["+kekbot.name+"] Disabled.");
-		kekbot.enabled = false;	
-	}
-	else{
-		console.log("KekBot is already disabled!");
-	}
-}
+
 kekbot.handleCommand = function(data){
 	if (data.type != "message"){
 		return false;
 	}
-
-	switch(data.message){
+	data.message = data.message.split(" ");
+	switch(data.message[0]){
+		case "%enable":
+			kekbot.test.ifMod(data, kekbot.handle.enable, true);
+			break;
+		case "%disable":
+			kekbot.test.ifMod(data, kekbot.handle.disable, true);
+			break;
 		case "%roll":
-			kekbot.handle.roll(data);
+			kekbot.test.ifEnabled(data, kekbot.handle.roll);
+			break;
+		case "%addmod":
+			kekbot.test.ifEnabled(data, function(data){kekbot.test.ifMod(data, kekbot.handle.addmod, true)});
 			break;
 		default:
 			break;
 	}
 }
+
+kekbot.say = function(msg){
+	$("#chat-input-field").val(msg).trigger($.Event("keydown",{keyCode: 13}));
+}
+
+kekbot.test = {};
+kekbot.test.ifEnabled = function(data, func){
+	if (kekbot.enabled){
+		func(data);
+	}
+}
+kekbot.test.ifMod = function(data, func, admin){
+	if (admin){
+		if (kekbot.mods[data.from] && kekbot.mods[data.from] == "admin"){
+			func(data);
+		}
+	}
+	else{
+		if (kekbot.mods[data.from]){
+			func(data);
+		}
+		else{
+			kekbot.say("You're not privileged! @"+data.from);
+		}	
+	}
+}
+
+
 kekbot.handle = {};
+kekbot.handle.enable = function(){
+	if(!kekbot.enabled){
+		kekbot.say("["+kekbot.name+"] Enabled.");
+		kekbot.enabled = true;
+	}
+	else{
+		kekbot.say("KekBot is already enabled!");
+	}
+}
+kekbot.handle.disable = function(){
+	if(kekbot.enabled){
+		kekbot.say("["+kekbot.name+"] Disabled.");
+		kekbot.enabled = false;	
+	}
+	else{
+		kekbot.say("KekBot is already disabled!");
+	}
+}
 kekbot.handle.roll = function(data){
 	kekbot.say("Roll: "+Math.floor(Math.random()*999999)+" @"+data.from);
 }
+kekbot.handle.addmod = function(data){
+	data.message[1] = data.message[1].split('"')[0];
+	kekbot.mods[data.message[1]] = true;
+	kekbot.say("@"+data.from+": Added "+data.message[1]+" to the modlist.");
+}
+kekbot.init();
