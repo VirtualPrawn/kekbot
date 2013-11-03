a2a68daa6233d82e831a6a087aa58cfbbc09602a
ivan <strategetical@gmail.com>
2013-11-02 19:30:45 -0500

    enable_checkers We don't want the bot to be enabled/disabled many times, because if we enable the bot multiple times, we won't be able to disable it unless we refresh the page.

--- kekbot.js
+++ kekbot.js
@@ -1,14 +1,27 @@
 var kekbot = {};
+kekbot.enabled = false;
 kekbot.enable = function(){
-	API.on(API.CHAT, kekbot.handleCommand);
-	kekbot.say("[bot] Enabled.");
+	if(!kekbot.enabled){
+		API.on(API.CHAT, kekbot.handleCommand);
+		kekbot.say("[bot] Enabled.");
+		kekbot.enabled = true;	
+	}
+	else{
+		console.log("KekBot is already enabled!");
+	}
 }
 kekbot.say = function(msg){
 	$("#chat-input-field").val(msg).trigger($.Event("keydown",{keyCode: 13}));
 }
 kekbot.disable = function(){
-	API.off(API.CHAT, kekbot.handleCommand);
-	kekbot.say("[bot] Disabled.");
+	if(kekbot.enabled){
+		API.off(API.CHAT, kekbot.handleCommand);
+		kekbot.say("[bot] Disabled.");
+		kekbot.enabled = false;	
+	}
+	else{
+		console.log("KekBot is already disabled!");
+	}
 }
 kekbot.handleCommand = function(data){
 	if (data.type != "message"){
