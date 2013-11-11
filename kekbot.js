e5a728540f14ce075f55b904d6337113c0e741db
VirtualPrawn <virtualprawn@hotmail.com>
2013-11-10 17:35:59 -0800

    Update kekbot.js

--- kekbot.js
+++ kekbot.js
@@ -9,7 +9,7 @@ kekbot.version = "2.0_SexBotEdition";
 //Bot mods.
 kekbot.mods = {};
 kekbot.mods["SexBot"] = "admin";
-kekbot.mods["SexBot"] = "admin";
+kekbot.mods["Sexual_Lobster"] = "admin";
 
 //Bot fortunes.
 kekbot.stairs = [
