3ccb8806586df5b1016e29052854201b88b21f95
VirtualPrawn <VirtualPrawn@users.noreply.github.com>
2013-11-16 23:13:22 -0800

    Update kekbot.js
    last didnt werk

--- kekbot.js
+++ kekbot.js
@@ -358,7 +358,7 @@ kekbot.readmsg = function(data){
 		kekbot.enabled&&
 		kekbot.say_raw("/me | cool ranch or get ded ");		
 	}
-	else if (data.message == "jb"){
+	else if (data.message == "Jason Brazil"){
 		kekbot.enabled&&
 		kekbot.say_raw("/me | We'll I live the Beatles, for she reason I'm obsessed with Jason Brazil");		
 	}
